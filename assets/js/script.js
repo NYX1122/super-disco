10ed429bd002787e3aad5d17d4e4274732093313
BoDee <nyx0hemir@gmail.com>
2021-05-29 14:10:49 -0600

    finished adding key elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,21 @@
 var date = $("#currentDay").text("placeholder");
 
 i = 0;
-while (i < 8) {
+while (i < 9) {
     var hourRowWrapper = $("<div>").addClass("row");
     if(i < 3) {
-        var hour = $("<p>").text((9 + i) + "AM").addClass("col-1 border-top border-end border-bottom border-dark mb-0");
+        var hour = $("<p>").text((9 + i) + "AM").addClass("col-1 list-group-item list-group-item-primary mb-0");
     }
     else if(i === 3){
-        var hour = $("<p>").text(12 + "PM").addClass("col-1 border-top border-end border-bottom border-dark mb-0");
+        var hour = $("<p>").text(12 + "PM").addClass("col-1 list-group-item list-group-item-primary mb-0");
     }
     else {
-        var hour = $("<p>").text((i - 3) + "PM").addClass("col-1 border-top border-end border-bottom border-dark mb-0");
+        var hour = $("<p>").text((i - 3) + "PM").addClass("col-1 list-group-item list-group-item-primary mb-0");
     };
     var taskDiv = $("<div>").addClass("col-10 list-group-item list-group-item-secondary").attr("id", "task");
     var taskText = $("<p>").addClass("fw-b").text("placeholder");
     taskDiv.append(taskText);
-    var button = $("<button>").text("placeholder").addClass("col-1");
+    var button = $("<button>").html("<span class='oi oi-lock-locked'></span>").addClass("col-1 btn btn-info");
     hourRowWrapper.append(hour, taskDiv, button);
     $(".container").append(hourRowWrapper);
     i++;
