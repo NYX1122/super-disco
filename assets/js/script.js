87fe71aa95b91a10b1d89660b52101d9bda68847
BoDee <nyx0hemir@gmail.com>
2021-05-29 15:40:50 -0600

    trying to figure out time logic, very tough

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,50 @@
-var date = $("#currentDay").text("placeholder");
+var date = $("#currentDay").text(moment().format('dddd') + ", " + moment().format('MMMM Do'));
+setInterval(function() {
+    var date = $("#currentDay").text(moment().format('dddd') + ", " + moment().format('MMMM Do'));
+}, 60000);
+
+var timeCheck = function (task, taskInfo) {
+    taskTime = task.text();
+    taskTime = taskTime.replace("AM", "");
+    taskTime = taskTime.replace("PM", "");
+    taskIndicator = task.text();
+    taskIndicator = taskIndicator.replace(/[0-9]/g, "");
+    //timeIndicator = moment().format('A');
+    timeIndicator = "PM";
+    time = 12;
+    //time = moment().format("h");
+    if (time === 12) {
+        if (taskIndicator === "PM" && taskTime < 6) {
+            taskInfo.removeClass("list-group-item-primary").addClass("list-group-item-success");
+        }
+        else if (taskTime === 12) {
+            taskInfo.removeClass("list-group-item-primary").addClass("list-group-item-danger");
+        }
+    }
+    else if(taskIndicator === "PM" && timeIndicator === "PM") {
+        if (time < taskTime) {
+            taskInfo.removeClass("list-group-item-primary").addClass("list-group-item-success");
+        }
+        else if (time > taskTime) {
+        }
+        else {
+            taskInfo.removeClass("list-group-item-primary").addClass("list-group-item-danger");
+        }
+    }
+    else if(taskIndicator === "AM" && timeIndicator === "AM") {
+        if (time < taskTime) {
+            taskInfo.removeClass("list-group-item-primary").addClass("list-group-item-success");
+        }
+        else if (time > taskTime) {
+        }
+        else {
+            taskInfo.removeClass("list-group-item-primary").addClass("list-group-item-danger");
+        }
+    }
+    else if(taskIndicator === "PM" && timeIndicator === "AM") {
+        taskInfo.removeClass("list-group-item-primary").addClass("list-group-item-success");
+    }
+};
 
 i = 0;
 while (i < 9) {
@@ -18,5 +64,8 @@ while (i < 9) {
     var button = $("<button>").html("<span class='oi oi-lock-locked'></span>").addClass("col-1 btn btn-info");
     hourRowWrapper.append(hour, taskDiv, button);
     $(".container").append(hourRowWrapper);
+    timeCheck(hour, taskDiv);
     i++;
 }
+
+console.log(moment().format('A'));
